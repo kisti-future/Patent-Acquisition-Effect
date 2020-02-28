ab139e1f0a301501d9a8ee47848cf76799bd3f64
so-young-kim <52688617+so-young-kim@users.noreply.github.com>
2020-02-28 09:46:01 +0900

    Create TEMP_R1_MATCHED_ALL_UTILITY.sql

---
+++ TABLES/TEMP_R1_MATCHED_ALL_UTILITY.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+--  DDL for Table TEMP_R1_MATCHED_ALL_UTILITY
+--------------------------------------------------------
+
+  CREATE TABLE "TEMP_R1_MATCHED_ALL_UTILITY" 
+   (	"PERSON_ID" NUMBER(11,0), 
+	"PERSON_NAME" VARCHAR2(4000), 
+	"PAT_PUBLN_ID" NUMBER(11,0), 
+	"PUBLN_NR" VARCHAR2(15), 
+	"APPLN_ID" NUMBER(11,0), 
+	"PSN_SECTOR" VARCHAR2(500)
+   )
