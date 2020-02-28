af36dc4a3612e30a3a9ec245fb1be6c63cccaa38
so-young-kim <52688617+so-young-kim@users.noreply.github.com>
2020-02-28 09:34:13 +0900

    Create TEMP_R1_ALL_APPLT_ASGN_MATCH.sql

---
+++ TABLES/TEMP_R1_ALL_APPLT_ASGN_MATCH.sql
@@ -0,0 +1,9 @@
+--------------------------------------------------------
+--  DDL for Table TEMP_R1_ALL_APPLT_ASGN_MATCH
+--------------------------------------------------------
+
+  CREATE TABLE "TEMP_R1_ALL_APPLT_ASGN_MATCH" 
+   (	"NAME" VARCHAR2(1000), 
+	"PERSON_NAME" VARCHAR2(4000), 
+	"PERSON_ID" NUMBER(11,0)
+   )
