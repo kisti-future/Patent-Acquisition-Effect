2daa9b0acaa6190b25332d091040b7a5f07fdd04
so-young-kim <52688617+so-young-kim@users.noreply.github.com>
2020-02-28 09:45:52 +0900

    Create TEMP_R1_IND_SELF_CITED.sql

---
+++ TABLES/TEMP_R1_IND_SELF_CITED.sql
@@ -0,0 +1,11 @@
+--------------------------------------------------------
+--  DDL for Table TEMP_R1_IND_SELF_CITED
+--------------------------------------------------------
+
+  CREATE TABLE "TEMP_R1_IND_SELF_CITED" 
+   (	"DOC_NUMBER" VARCHAR2(1000), 
+	"NAME" VARCHAR2(1000), 
+	"PERSON_NAME" VARCHAR2(4000), 
+	"PUBLN_NR" VARCHAR2(15), 
+	"REEL_FRAME_NO" NUMBER
+   )
